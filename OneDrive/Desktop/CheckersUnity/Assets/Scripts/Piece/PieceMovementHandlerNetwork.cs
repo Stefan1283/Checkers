be36c93894bdd59b51977b32bf60d96a868ff08b
Arthur G. <arturgamidov2705@gmail.com>
2024-04-10 19:46:50 +0300

    Added movement for checkers (White).

--- OneDrive/Desktop/CheckersUnity/Assets/Scripts/Piece/PieceMovementHandlerNetwork.cs
+++ OneDrive/Desktop/CheckersUnity/Assets/Scripts/Piece/PieceMovementHandlerNetwork.cs
@@ -6,5 +6,35 @@ using UnityEngine;
 
 public class PieceMovementHandlerNetwork : PieceMovementHandler
 {
-    
+    public override void OnStartAuthority()
+    {
+        TilesSelectionHandler.OnTileSelected += HandleTileSelected;
+    }
+
+    public override void OnStopClient()
+    {
+        TilesSelectionHandler.OnTileSelected -= HandleTileSelected;
+    }
+
+    protected override void Move(Vector3 position, bool nextTurn)
+    {
+        CMDMove(position, nextTurn);
+    }
+
+    [Command]
+    void CMDMove(Vector3 position, bool nextTurn)
+    {
+        base.Move(position, nextTurn);
+    }
+
+    protected override void Capture(Vector2Int piecePosition)
+    {
+        CMDCapture(piecePosition);
+    }
+
+    [Command]
+    void CMDCapture(Vector2Int piecePosition)
+    {
+        base.Capture(piecePosition);
+    }
 }
